80e9b74e7a21043097096e8e7a13857f7bf87003
Serikarakhil1209 <serikarakhil5@gmail.com>
2025-02-20 09:05:34 +0530

    first commit

---
+++ DAY1/datatype.js
@@ -0,0 +1,29 @@
+alert('hi')
+
+let l1="akhil";
+var l2="akki";
+const l3="man";
+console.log("l1")
+console.log("l2")
+console.log("l3")
+
+let num=2;
+let name="akhil"
+let single=true;
+var name1;
+let age=null;
+let object1={
+    name5:"akhil"
+}
+Array1=[1,2,3,4,"akki"]
+console.log("num")
+console.log("name")
+console.log("single")
+console.log("name1")
+console.log("age")
+console.log("object1")
+
+function name6(){
+    console.log("this is a function")
+}
+name6()
