dee6aad2feea2f6fece597290e6b7a298bb0c1be
Serikarakhil1209 <serikarakhil5@gmail.com>
2025-03-18 08:48:34 +0530

    map filter

---
+++ day-transformation/map.js
@@ -0,0 +1,37 @@
+// arr=[1,2,3,4,5,56]
+
+// let newarr = arr.map((val)=>{
+//     return val*2
+// })
+// console.log(newarr)
+
+// arr=[1,2,3,4,5,56]
+
+// let newarr = arr.map((val)=>{
+//     return val**2
+// })
+// console.log(newarr)
+
+// arr=[1,2,3,4,5,56]
+
+// let newarr = arr.filter((val)=>{
+//     return (val%2==0 && val < 50)
+// })
+// console.log(newarr)
+
+// arr=[1,2,3,4,5,56]
+
+// let newarr = arr.filter((val)=>{
+//     return (val%2==0 && val < 50)
+// })
+// console.log(newarr)
+
+// arr=["akhil1","akki","bhai"]
+
+// let newarr = arr.filter((val)=>{
+//     console.log(val.length > 5 )
+//     // return val.length > 5
+// })
+// console.log(newarr)
+
+
